b20c28321672359b808a0362f54b430af333834b
RJ Ortiz <roggerjortiz@gmail.com>
2023-03-03 20:06:31 -0500

    Sensitive Files

--- reboot/rebootMMC.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo rjanortiz | sudo screen -S MMC -p 0 -X stuff 'stop\n'
-sleep 5
-echo rjanortiz | sudo screen -S MMC -p 0 -X stuff './run.sh\n'
--- reboot/rollback.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cp ~/WIP/entity/backup.py ~/boot/NTT/entity.py
-
-echo rjanortiz | sudo screen -S NTT -p 0 -X stuff '^C\n'
-echo rjanortiz | sudo screen -S NTT -p 0 -X stuff 'python3 entity.py\n'
